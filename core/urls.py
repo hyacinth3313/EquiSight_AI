2cc145eb8cccabb6e25476ca32d69c0004a35196
hyacinth3313 <163036003+hyacinth3313@users.noreply.github.com>
2026-04-28 22:43:03 +0530

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,7 @@ from auditor import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index_view, name='index'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
-    # THIS LINE IS MISSING OR MISSPELLED:
+    path('', views.index_view, name='login'),           # Changed to index_view
+    path('dashboard/', views.dashboard_view, name='dashboard'), # Changed to dashboard_view
     path('download/', views.download_csv, name='download_csv'), 
 ]
